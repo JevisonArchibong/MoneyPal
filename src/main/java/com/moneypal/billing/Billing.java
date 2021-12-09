60b4e1fa0003655b7a1798646f8eb7c8945be1be
JevisonArchibong <jevison.archibong@exquisappfactory.com>
2021-12-09 18:19:49 +0100

    Billing Commit

--- src/main/java/com/moneypal/billing/Billing.java
+++ src/main/java/com/moneypal/billing/Billing.java
@@ -11,13 +11,23 @@
  */
 package com.moneypal.billing;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Jevison7x
  * @since Dec 9, 2021 4:57:57 PM
  */
-public class Billing
+@Entity
+public class Billing implements Serializable
 {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String userName;
     private String invoiceNo;
